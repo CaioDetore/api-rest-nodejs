c6407df7c4be7e7dae48750725cb61c778c9cead
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-04-07 12:51:43 -0300

    feat: utilizando cookies no Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import fatify from "fastify";
+import cookies from "@fastify/cookie";
 import { env } from "./env";
 import { transactionsRouter } from "./routes/transactions";
 
 const app = fatify();
+
+app.register(cookies);
 
 app.register(transactionsRouter, {
   prefix: "/transactions",
